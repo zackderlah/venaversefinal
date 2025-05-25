cdcee9b852d74090ce5e618c014310b93943aa6e
zackderlah <zackloot@gmail.com>
2025-05-25 19:33:36 +0700

    fix(types): add isAdmin to NextAuth session and user types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,6 +7,7 @@ declare module "next-auth" {
       name?: string | null;
       email?: string | null;
       image?: string | null;
+      isAdmin?: boolean;
     };
   }
 
@@ -15,5 +16,6 @@ declare module "next-auth" {
     name?: string | null;
     email?: string | null;
     image?: string | null;
+    isAdmin?: boolean;
   }
 } 
